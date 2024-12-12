4ede1f4b452aeeaecfa8bb2e40e4de65aff9d122
Thiago Elias <thiagoelias99@gmail.com>
2024-12-12 15:19:41 -0300

    feat: :zap: List variable incomes

--- planner-web-next/src/app/(authenticated)/patrimonio/renda-variavel/page.tsx
+++ planner-web-next/src/app/(authenticated)/patrimonio/renda-variavel/page.tsx
@@ -3,14 +3,24 @@
 import { Button } from "@/components/ui/button"
 import { H1, H2 } from "@/components/ui/typography"
 import { useAssets } from "@/hooks/use-assets"
-import { ChevronLeftIcon } from "lucide-react"
+import { ChevronLeftIcon, Loader2Icon } from "lucide-react"
 import { useRouter } from "next/navigation"
 import AutoUpdateTickersButton from "../_partials/auto-update-tickers-button"
 import StocksTable from "./_partials/stocks-table"
+import { tickerTypeMapper } from "../_utils"
+import { TickerType } from "@prisma/client"
+import { useMemo } from "react"
 
 export default function RendaVariavelPage() {
   const { assets, isLoadingAssets } = useAssets()
   const router = useRouter()
+
+  const stocks = useMemo(() => assets?.variableIncome.stocks, [assets])
+  const reits = useMemo(() => assets?.variableIncome.reits, [assets])
+  const internationalStocks = useMemo(() => assets?.variableIncome.internationalStocks, [assets])
+  const cryptos = useMemo(() => assets?.variableIncome.cryptos, [assets])
+  const golds = useMemo(() => assets?.variableIncome.golds, [assets])
+  const etfs = useMemo(() => assets?.variableIncome.etfs, [assets])
 
   return (
     <div className='py-4 max-w-screen-2xl mx-auto'>
@@ -21,12 +31,50 @@ export default function RendaVariavelPage() {
         </div>
         <AutoUpdateTickersButton />
       </div>
-      <div className='pt-6'>
-        <div className="space-y-2">
-          <H2>Ações</H2>
-          <StocksTable stocks={assets?.variableIncome.stocks} isLoading={isLoadingAssets} />
+      {isLoadingAssets ? (
+        <div className="w-full flex justify-center items-center">
+          <Loader2Icon className='w-8 h-8 animate-spin' />
         </div>
-      </div>
+      ) : (
+        <div className='pt-6 space-y-8'>
+          {(stocks?.length || 0) > 0 && (
+            <div className="space-y-2">
+              <H2>{tickerTypeMapper[TickerType.STOCK].label}</H2>
+              <StocksTable stocks={stocks} isLoading={isLoadingAssets} />
+            </div>
+          )}
+          {(reits?.length || 0) > 0 && (
+            <div className="space-y-2">
+              <H2>{tickerTypeMapper[TickerType.REIT].label}</H2>
+              <StocksTable stocks={reits} isLoading={isLoadingAssets} />
+            </div>
+          )}
+          {(internationalStocks?.length || 0) > 0 && (
+            <div className="space-y-2">
+              <H2>{tickerTypeMapper[TickerType.INTERNATIONAL].label}</H2>
+              <StocksTable stocks={internationalStocks} isLoading={isLoadingAssets} />
+            </div>
+          )}
+          {(cryptos?.length || 0) > 0 && (
+            <div className="space-y-2">
+              <H2>{tickerTypeMapper[TickerType.CRYPTO].label}</H2>
+              <StocksTable stocks={cryptos} isLoading={isLoadingAssets} />
+            </div>
+          )}
+          {(golds?.length || 0) > 0 && (
+            <div className="space-y-2">
+              <H2>{tickerTypeMapper[TickerType.GOLD].label}</H2>
+              <StocksTable stocks={golds} isLoading={isLoadingAssets} />
+            </div>
+          )}
+          {(etfs?.length || 0) > 0 && (
+            <div className="space-y-2">
+              <H2>{tickerTypeMapper[TickerType.ETF].label}</H2>
+              <StocksTable stocks={etfs} isLoading={isLoadingAssets} />
+            </div>
+          )}
+        </div>
+      )}
     </div>
   )
 }
--- planner-web-next/src/models/assets/variable-income.ts
+++ planner-web-next/src/models/assets/variable-income.ts
@@ -3,10 +3,20 @@ import { Stock, Ticker, TickerOrderWithMeanPrice } from "."
 
 export interface IVariableIncome {
   stocks: Stock[]
+  etfs: Stock[]
+  reits: Stock[]
+  golds: Stock[]
+  cryptos: Stock[]
+  internationalStocks: Stock[]
 }
 
 export class VariableIncome implements IVariableIncome {
   stocks: Stock[]
+  etfs: Stock[]
+  reits: Stock[]
+  golds: Stock[]
+  cryptos: Stock[]
+  internationalStocks: Stock[]
 
   constructor(tickers: Ticker[], orders: TickerOrderWithMeanPrice[]) {
     //Get last order from each ticker
@@ -22,8 +32,19 @@ export class VariableIncome implements IVariableIncome {
 
     //Filter orders with quantity different from 0 and return a Stock object
     const ordersWithNomZeroQuantity = lastOrdersFromEachTicker.filter(order => order.newTotalQuantity !== 0)
-    this.stocks = ordersWithNomZeroQuantity
-      .filter(order => order.tickerType === TickerType.STOCK)
+
+    //Get by type
+    this.stocks = this.getStocksByType(ordersWithNomZeroQuantity, tickers, TickerType.STOCK)
+    this.etfs = this.getStocksByType(ordersWithNomZeroQuantity, tickers, TickerType.ETF)
+    this.reits = this.getStocksByType(ordersWithNomZeroQuantity, tickers, TickerType.REIT)
+    this.golds = this.getStocksByType(ordersWithNomZeroQuantity, tickers, TickerType.GOLD)
+    this.cryptos = this.getStocksByType(ordersWithNomZeroQuantity, tickers, TickerType.CRYPTO)
+    this.internationalStocks = this.getStocksByType(ordersWithNomZeroQuantity, tickers, TickerType.INTERNATIONAL)
+  }
+
+  private getStocksByType(orders: TickerOrderWithMeanPrice[], tickers: Ticker[], type: TickerType): Stock[] {
+    return orders
+      .filter(order => order.tickerType === type)
       .map(order => new Stock(
         tickers.find(ticker => ticker.symbol === order.ticker)!,
         order
