6093694700cbfd7b2d006df8813b40a008f82918
Thiago Elias <thiagoelias99@gmail.com>
2024-12-27 15:15:10 -0300

    lint fix

--- planner-web-next/src/app/(authenticated)/patrimonio/page.tsx
+++ planner-web-next/src/app/(authenticated)/patrimonio/page.tsx
@@ -16,92 +16,92 @@ export default function PatrimonioPage() {
   const router = useRouter()
 
   return (
-    // <div>
-    //   <h1>Patrimônio - {formatCurrency(assets?.summary.totalAmount)}</h1>
-    //   <div className="flex gap-4">
-    //     <Link
-    //       href={"patrimonio/tickers"}
-    //       className={buttonVariants({
-    //         className: "mt-4",
-    //       })}
-    //     >
-    //       Tickers
-    //     </Link>
-    //     <Link
-    //       href={"patrimonio/tickers/ordens"}
-    //       className={buttonVariants({
-    //         className: "mt-4",
-    //       })}
-    //     >
-    //       Ordens
-    //     </Link>
-    //     <Link
-    //       href={"patrimonio/renda-variavel"}
-    //       className={buttonVariants({
-    //         className: "mt-4",
-    //       })}
-    //     >
-    //       Renda Variável
-    //     </Link>
-    //     <Link
-    //       href={"patrimonio/renda-fixa"}
-    //       className={buttonVariants({
-    //         className: "mt-4",
-    //       })}
-    //     >
-    //       Renda Fixa
-    //     </Link>
-    //     <Link
-    //       href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.CASH_BOX].slug}`}
-    //       className={buttonVariants({
-    //         className: "mt-4",
-    //       })}
-    //     >
-    //       {otherAssetsTypeMapper[OthersAssetsTypes.CASH_BOX].label}
-    //     </Link>
-    //     <Link
-    //       href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.FINANCIAL_INJECTION].slug}`}
-    //       className={buttonVariants({
-    //         className: "mt-4",
-    //       })}
-    //     >
-    //       {otherAssetsTypeMapper[OthersAssetsTypes.FINANCIAL_INJECTION].label}
-    //     </Link>
-    //     <Link
-    //       href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.PENSION].slug}`}
-    //       className={buttonVariants({
-    //         className: "mt-4",
-    //       })}
-    //     >
-    //       {otherAssetsTypeMapper[OthersAssetsTypes.PENSION].label}
-    //     </Link>
-    //     <Link
-    //       href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.PROPERTY].slug}`}
-    //       className={buttonVariants({
-    //         className: "mt-4",
-    //       })}
-    //     >
-    //       {otherAssetsTypeMapper[OthersAssetsTypes.PROPERTY].label}
-    //     </Link>
-    //     <Link
-    //       href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.SHARE].slug}`}
-    //       className={buttonVariants({
-    //         className: "mt-4",
-    //       })}
-    //     >
-    //       {otherAssetsTypeMapper[OthersAssetsTypes.SHARE].label}
-    //     </Link>
-    //     <Link
-    //       href={"patrimonio/atualizacao-em-massa"}
-    //       className={buttonVariants({
-    //         className: "mt-4",
-    //       })}
-    //     >
-    //       Atualização em Massa
-    //     </Link>
-    //   </div>
-    //   {/* <pre>{JSON.stringify(assets, null, 2)}</pre> */}
-    // </div>
+  // <div>
+  //   <h1>Patrimônio - {formatCurrency(assets?.summary.totalAmount)}</h1>
+  //   <div className="flex gap-4">
+  //     <Link
+  //       href={"patrimonio/tickers"}
+  //       className={buttonVariants({
+  //         className: "mt-4",
+  //       })}
+  //     >
+  //       Tickers
+  //     </Link>
+  //     <Link
+  //       href={"patrimonio/tickers/ordens"}
+  //       className={buttonVariants({
+  //         className: "mt-4",
+  //       })}
+  //     >
+  //       Ordens
+  //     </Link>
+  //     <Link
+  //       href={"patrimonio/renda-variavel"}
+  //       className={buttonVariants({
+  //         className: "mt-4",
+  //       })}
+  //     >
+  //       Renda Variável
+  //     </Link>
+  //     <Link
+  //       href={"patrimonio/renda-fixa"}
+  //       className={buttonVariants({
+  //         className: "mt-4",
+  //       })}
+  //     >
+  //       Renda Fixa
+  //     </Link>
+  //     <Link
+  //       href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.CASH_BOX].slug}`}
+  //       className={buttonVariants({
+  //         className: "mt-4",
+  //       })}
+  //     >
+  //       {otherAssetsTypeMapper[OthersAssetsTypes.CASH_BOX].label}
+  //     </Link>
+  //     <Link
+  //       href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.FINANCIAL_INJECTION].slug}`}
+  //       className={buttonVariants({
+  //         className: "mt-4",
+  //       })}
+  //     >
+  //       {otherAssetsTypeMapper[OthersAssetsTypes.FINANCIAL_INJECTION].label}
+  //     </Link>
+  //     <Link
+  //       href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.PENSION].slug}`}
+  //       className={buttonVariants({
+  //         className: "mt-4",
+  //       })}
+  //     >
+  //       {otherAssetsTypeMapper[OthersAssetsTypes.PENSION].label}
+  //     </Link>
+  //     <Link
+  //       href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.PROPERTY].slug}`}
+  //       className={buttonVariants({
+  //         className: "mt-4",
+  //       })}
+  //     >
+  //       {otherAssetsTypeMapper[OthersAssetsTypes.PROPERTY].label}
+  //     </Link>
+  //     <Link
+  //       href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.SHARE].slug}`}
+  //       className={buttonVariants({
+  //         className: "mt-4",
+  //       })}
+  //     >
+  //       {otherAssetsTypeMapper[OthersAssetsTypes.SHARE].label}
+  //     </Link>
+  //     <Link
+  //       href={"patrimonio/atualizacao-em-massa"}
+  //       className={buttonVariants({
+  //         className: "mt-4",
+  //       })}
+  //     >
+  //       Atualização em Massa
+  //     </Link>
+  //   </div>
+  //   {/* <pre>{JSON.stringify(assets, null, 2)}</pre> */}
+  // </div>
 
     <div className='py-4 max-w-screen-2xl mx-auto'>
       <div className="w-full flex flex-col sm:flex-row justify-between items-baseline gap-4">
