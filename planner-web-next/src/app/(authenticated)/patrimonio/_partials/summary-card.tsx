fd6fc8f20709cc47968980bf8f65c304437d6cff
Thiago Elias <thiagoelias99@gmail.com>
2024-12-27 15:05:58 -0300

    up

---
+++ planner-web-next/src/app/(authenticated)/patrimonio/_partials/summary-card.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+} from "@/components/ui/card"
+import { Skeleton } from "@/components/ui/skeleton"
+import { cn } from "@/lib/utils"
+import { LucideIcon } from "lucide-react"
+import { useEffect, useState } from "react"
+import CountUp from "react-countup"
+import { ClassNameValue } from "tailwind-merge"
+
+interface Props {
+  title: string
+  amount?: number | undefined | null
+  Icon?: LucideIcon
+  iconClassName?: ClassNameValue
+  useSecondaryBackground?: boolean
+  amountTextClassName?: ClassNameValue
+  isLoading?: boolean
+  className?: ClassNameValue
+}
+
+export default function SummaryCard({ title, amount = 0, Icon, useSecondaryBackground, className, amountTextClassName, iconClassName, isLoading }: Props) {
+  const [progress, setProgress] = useState({
+    previous: 0,
+    current: 0
+  })
+
+  useEffect(() => {
+    setProgress(curr => {
+      return {
+        previous: curr.current,
+        current: amount || 0
+      }
+    })
+  }, [amount])
+
+  return (
+    <Card className={cn("w-full", useSecondaryBackground && "bg-transparent", className)}>
+      <CardHeader className="flex flex-row justify-start items-center gap-4 pb-2">
+        {Icon && (
+          <div className="bg-muted p-2 rounded-md">
+            <Icon size={24} className={cn("", iconClassName)} />
+          </div>
+        )}
+        <h2 className="text-muted-foreground text-sm">{title}</h2>
+      </CardHeader>
+      <CardContent className="flex justify-center items-center">
+        {isLoading ? (
+          <Skeleton><p className={cn("mt-2 text-base text-transparent", amountTextClassName)}>
+            R$ 999999,99</p></Skeleton>
+        ) : (
+          <p className='mt-2 text-base text-muted-foreground'>
+            R$ <CountUp start={progress.previous} end={progress.current} duration={1} decimals={2} separator=" " decimal="," className={cn("text-foreground font-medium text-3xl sm:text-xl", amountTextClassName)} /></p>
+        )}
+      </CardContent>
+    </Card>
+  )
+}
--- planner-web-next/src/app/(authenticated)/patrimonio/page.tsx
+++ planner-web-next/src/app/(authenticated)/patrimonio/page.tsx
@@ -1,101 +1,181 @@
 "use client"
 
-import { buttonVariants } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 import { useAssets } from "@/hooks/use-assets"
 import Link from "next/link"
 import { otherAssetsTypeMapper } from "./_utils"
 import { OthersAssetsTypes } from "@prisma/client"
 import { formatCurrency } from "@/lib/utils"
+import { ChevronLeftIcon, DollarSignIcon } from "lucide-react"
+import { H1 } from "@/components/ui/typography"
+import { useRouter } from "next/navigation"
+import SummaryCard from "./_partials/summary-card"
 
 export default function PatrimonioPage() {
   const { assets, isLoadingAssets } = useAssets()
+  const router = useRouter()
 
   return (
-    <div>
-      <h1>Patrimônio - {formatCurrency(assets?.summary.totalAmount)}</h1>
-      <div className="flex gap-4">
-        <Link
-          href={"patrimonio/tickers"}
-          className={buttonVariants({
-            className: "mt-4",
-          })}
-        >
-          Tickers
-        </Link>
-        <Link
-          href={"patrimonio/tickers/ordens"}
-          className={buttonVariants({
-            className: "mt-4",
-          })}
-        >
-          Ordens
-        </Link>
-        <Link
-          href={"patrimonio/renda-variavel"}
-          className={buttonVariants({
-            className: "mt-4",
-          })}
-        >
-          Renda Variável
-        </Link>
-        <Link
-          href={"patrimonio/renda-fixa"}
-          className={buttonVariants({
-            className: "mt-4",
-          })}
-        >
-          Renda Fixa
-        </Link>
-        <Link
-          href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.CASH_BOX].slug}`}
-          className={buttonVariants({
-            className: "mt-4",
-          })}
-        >
-          {otherAssetsTypeMapper[OthersAssetsTypes.CASH_BOX].label}
-        </Link>
-        <Link
-          href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.FINANCIAL_INJECTION].slug}`}
-          className={buttonVariants({
-            className: "mt-4",
-          })}
-        >
-          {otherAssetsTypeMapper[OthersAssetsTypes.FINANCIAL_INJECTION].label}
-        </Link>
-        <Link
-          href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.PENSION].slug}`}
-          className={buttonVariants({
-            className: "mt-4",
-          })}
-        >
-          {otherAssetsTypeMapper[OthersAssetsTypes.PENSION].label}
-        </Link>
-        <Link
-          href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.PROPERTY].slug}`}
-          className={buttonVariants({
-            className: "mt-4",
-          })}
-        >
-          {otherAssetsTypeMapper[OthersAssetsTypes.PROPERTY].label}
-        </Link>
-        <Link
-          href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.SHARE].slug}`}
-          className={buttonVariants({
-            className: "mt-4",
-          })}
-        >
-          {otherAssetsTypeMapper[OthersAssetsTypes.SHARE].label}
-        </Link>
+    // <div>
+    //   <h1>Patrimônio - {formatCurrency(assets?.summary.totalAmount)}</h1>
+    //   <div className="flex gap-4">
+    //     <Link
+    //       href={"patrimonio/tickers"}
+    //       className={buttonVariants({
+    //         className: "mt-4",
+    //       })}
+    //     >
+    //       Tickers
+    //     </Link>
+    //     <Link
+    //       href={"patrimonio/tickers/ordens"}
+    //       className={buttonVariants({
+    //         className: "mt-4",
+    //       })}
+    //     >
+    //       Ordens
+    //     </Link>
+    //     <Link
+    //       href={"patrimonio/renda-variavel"}
+    //       className={buttonVariants({
+    //         className: "mt-4",
+    //       })}
+    //     >
+    //       Renda Variável
+    //     </Link>
+    //     <Link
+    //       href={"patrimonio/renda-fixa"}
+    //       className={buttonVariants({
+    //         className: "mt-4",
+    //       })}
+    //     >
+    //       Renda Fixa
+    //     </Link>
+    //     <Link
+    //       href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.CASH_BOX].slug}`}
+    //       className={buttonVariants({
+    //         className: "mt-4",
+    //       })}
+    //     >
+    //       {otherAssetsTypeMapper[OthersAssetsTypes.CASH_BOX].label}
+    //     </Link>
+    //     <Link
+    //       href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.FINANCIAL_INJECTION].slug}`}
+    //       className={buttonVariants({
+    //         className: "mt-4",
+    //       })}
+    //     >
+    //       {otherAssetsTypeMapper[OthersAssetsTypes.FINANCIAL_INJECTION].label}
+    //     </Link>
+    //     <Link
+    //       href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.PENSION].slug}`}
+    //       className={buttonVariants({
+    //         className: "mt-4",
+    //       })}
+    //     >
+    //       {otherAssetsTypeMapper[OthersAssetsTypes.PENSION].label}
+    //     </Link>
+    //     <Link
+    //       href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.PROPERTY].slug}`}
+    //       className={buttonVariants({
+    //         className: "mt-4",
+    //       })}
+    //     >
+    //       {otherAssetsTypeMapper[OthersAssetsTypes.PROPERTY].label}
+    //     </Link>
+    //     <Link
+    //       href={`patrimonio/${otherAssetsTypeMapper[OthersAssetsTypes.SHARE].slug}`}
+    //       className={buttonVariants({
+    //         className: "mt-4",
+    //       })}
+    //     >
+    //       {otherAssetsTypeMapper[OthersAssetsTypes.SHARE].label}
+    //     </Link>
+    //     <Link
+    //       href={"patrimonio/atualizacao-em-massa"}
+    //       className={buttonVariants({
+    //         className: "mt-4",
+    //       })}
+    //     >
+    //       Atualização em Massa
+    //     </Link>
+    //   </div>
+    //   {/* <pre>{JSON.stringify(assets, null, 2)}</pre> */}
+    // </div>
+
+    <div className='py-4 max-w-screen-2xl mx-auto'>
+      <div className="w-full flex flex-col sm:flex-row justify-between items-baseline gap-4">
+        <div className="w-full flex justify-start items-center sm:gap-2">
+          <Button variant="ghost" size="icon" onClick={() => router.back()}><ChevronLeftIcon /></Button>
+          <H1 className="text-start w-full">Patrimônio</H1>
+        </div>
         <Link
           href={"patrimonio/atualizacao-em-massa"}
           className={buttonVariants({
-            className: "mt-4",
+            className: "",
           })}
         >
           Atualização em Massa
         </Link>
       </div>
-      <pre>{JSON.stringify(assets, null, 2)}</pre>
-    </div>
+      <div className='grid grid-cols-1 sm:grid-cols-12 gap-4 pt-2'>
+        <SummaryCard
+          title="Saldo"
+          className="sm:col-span-4"
+          isLoading={isLoadingAssets}
+          amount={assets?.summary.totalAmount}
+          Icon={DollarSignIcon}
+          amountTextClassName="sm:text-4xl" />
+        <SummaryCard
+          title="Caixa"
+          className="sm:col-span-2"
+          isLoading={isLoadingAssets}
+          amount={assets?.summary.cashBoxAmount}
+          useSecondaryBackground
+        />
+        <SummaryCard
+          title="Renda Fixa"
+          className="sm:col-span-2"
+          isLoading={isLoadingAssets}
+          amount={assets?.summary.fixedIncomeAmount}
+          useSecondaryBackground
+        />
+        <div className="sm:col-span-4 sm:col-start-9 sm:row-start-1 sm:row-span-2 w-full h-full flex sm:block border rounded-lg justify-center items-center">
+          <p>Gráfico 1</p>
+        </div>
+
+        <SummaryCard
+          title="Renda Variável"
+          className="sm:col-span-2"
+          isLoading={isLoadingAssets}
+          amount={assets?.summary.variableIncomeAmount}
+          useSecondaryBackground
+        />
+        <SummaryCard
+          title="Previdência"
+          className="sm:col-span-2"
+          isLoading={isLoadingAssets}
+          amount={assets?.summary.pensionAmount}
+          useSecondaryBackground
+        />
+        <SummaryCard
+          title="Posses"
+          className="sm:col-span-2"
+          isLoading={isLoadingAssets}
+          amount={assets?.summary.propertyAmount}
+          useSecondaryBackground
+        />
+        <div className="sm:col-span-2 border rounded-lg">
+          <p>Share: {assets?.summary.shareAmount}</p>
+          <p>Aporte: {assets?.summary.financialInjectionAmount}</p>
+        </div>
+        <div className="sm:col-span-10 w-full h-80 flex sm:block border rounded-lg justify-center items-center">
+          <p>Gráfico 2</p>
+        </div>
+        <div className="sm:col-span-2 w-full flex sm:block border rounded-lg justify-center items-center">
+
+        </div>
+      </div>
+    </div >
   )
 }
