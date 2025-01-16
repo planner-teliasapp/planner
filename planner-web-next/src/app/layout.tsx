c2c388c78e37e88a795a096402f187190d2448cc
Thiago Elias <thiagoelias99@gmail.com>
2025-01-16 08:01:15 -0300

    speedy

--- planner-web-next/src/app/layout.tsx
+++ planner-web-next/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css"
 import QueryContext from "@/contexts/query-context"
 import { Mulish } from "next/font/google"
@@ -25,6 +26,7 @@ export default function RootLayout({
         className={`${mulish.className} antialiased`}
       >
         <Analytics />
+        <SpeedInsights />
         <QueryContext>
           {children}
           <Toaster />
