781633409bfd4a1cf5c6a1d5bff85777fe01723b
Thiago Elias <thiagoelias99@gmail.com>
2025-01-28 17:00:11 -0300

    mobile ui

--- planner-web-next/src/app/(authenticated)/orcamentos/recorrentes/[id]/page.tsx
+++ planner-web-next/src/app/(authenticated)/orcamentos/recorrentes/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { Caption, H1, H2, H3, H4 } from "@/components/ui/typography"
+import { Caption, H1, H2 } from "@/components/ui/typography"
 import { ChevronLeftIcon, DollarSignIcon } from "lucide-react"
 import { useRouter } from "next/navigation"
 import TransactionsTable from "./_partials/transactions-table"
@@ -9,7 +9,7 @@ import { useBudgets } from "@/hooks/use-budgets"
 import { useMemo } from "react"
 import SummaryCard from "../../mensal/_partials/summary-card"
 import { cn } from "@/lib/utils"
-import { paymentFrequencyMapper, transactionColorMapper, transactionIconMapper, transactionMapper } from "../../_utils"
+import { paymentFrequencyMapper, transactionMapper } from "../../_utils"
 import { TransactionFrequency, TransactionType } from "@prisma/client"
 import { format } from "date-fns"
 import CountUp from "react-countup"
@@ -67,9 +67,9 @@ export default function Page({ params }: Props) {
           <H1 className="text-start w-full">{recurringTransaction?.description}</H1>
         </div>
       </div>
-      <div className='grid grid-cols-2 sm:grid-cols-12 gap-4 pt-6'>
-        <SummaryCard title="Total" amount={transactionSummary.total} className="col-span-2 sm:col-span-3 max-h-40" Icon={DollarSignIcon} amountTextClassName="sm:text-4xl" isLoading={isLoadingTransactions || isLoadingRecurringTransactions} />
-        <div className="col-span-2 sm:col-span-3 border rounded-lg max-h-40">
+      <div className='grid grid-cols-2 sm:grid-cols-6 xl:grid-cols-12 gap-4 pt-6'>
+        <SummaryCard title="Total" amount={transactionSummary.total} className="col-span-2 sm:col-span-3 max-h-40" Icon={DollarSignIcon} amountTextClassName="sm:text-3xl xl:text-4xl" isLoading={isLoadingTransactions || isLoadingRecurringTransactions} />
+        <div className="col-span-2 sm:col-span-3 row-start-1 sm:row-start-auto border rounded-lg max-h-40">
           <div className="h-full flex flex-col justify-between items-start p-6">
             <H2>{transactionMapper[recurringTransaction?.type as TransactionType]?.label}</H2>
             <Caption className="text-muted-foreground">{paymentFrequencyMapper[recurringTransaction?.frequency as TransactionFrequency]?.label}</Caption>
@@ -79,28 +79,28 @@ export default function Page({ params }: Props) {
         <div className="col-span-2 border rounded-lg min-h-24 px-6 py-4 flex flex-col justify-between items-start gap-1 max-h-32">
           <p className="text-muted-foreground text-sm">Maior Valor</p>
           <p className='mt-2 text-base text-muted-foreground self-center'>
-            R$ <CountUp start={0} end={transactionSummary.greatestAmountTransaction?.amount || 0} duration={1} decimals={2} separator=" " decimal="," className={cn("text-foreground font-medium text-3xl sm:text-2xl")} /></p>
+            R$ <CountUp start={0} end={transactionSummary.greatestAmountTransaction?.amount || 0} duration={1} decimals={2} separator=" " decimal="," className={cn("text-foreground font-medium text-3xl xl:text-2xl")} /></p>
           {transactionSummary.greatestAmountTransaction?.date && <p className="text-xs text-muted-foreground">em {new Date(transactionSummary.greatestAmountTransaction?.date).toLocaleDateString()}</p>}
         </div>
         <div className="col-span-2 border rounded-lg min-h-24 px-6 py-4 flex flex-col justify-between items-start gap-1 max-h-32">
           <p className="text-muted-foreground text-sm">Menor Valor</p>
           <p className='mt-2 text-base text-muted-foreground self-center'>
-            R$ <CountUp start={0} end={transactionSummary.smallestAmountTransaction?.amount || 0} duration={1} decimals={2} separator=" " decimal="," className={cn("text-foreground font-medium text-3xl sm:text-2xl")} /></p>
+            R$ <CountUp start={0} end={transactionSummary.smallestAmountTransaction?.amount || 0} duration={1} decimals={2} separator=" " decimal="," className={cn("text-foreground font-medium text-3xl xl:text-2xl")} /></p>
           {transactionSummary.smallestAmountTransaction?.date && <p className="text-xs text-muted-foreground">em {new Date(transactionSummary.smallestAmountTransaction?.date).toLocaleDateString()}</p>}
         </div>
         <div className="col-span-2 border rounded-lg min-h-24 px-6 py-4 flex flex-col justify-between items-start gap-1 max-h-32">
           <p className="text-muted-foreground text-sm">Recorrências</p>
           <p className='mt-2 text-base text-muted-foreground self-center'>
-            <CountUp start={0} end={transactionSummary.count || 0} duration={1} decimals={0} separator=" " decimal="," className={cn("text-foreground font-medium text-3xl sm:text-2xl")} /></p>
-          {transactionSummary.first?.date && <p className="text-xs text-muted-foreground">{new Date(transactionSummary.first?.date).toLocaleDateString()} - {new Date(transactionSummary.last?.date || "").toLocaleDateString()}</p>}
+            <CountUp start={0} end={transactionSummary.count || 0} duration={1} decimals={0} separator=" " decimal="," className={cn("text-foreground font-medium text-3xl xl:text-2xl")} /></p>
+          {transactionSummary.first?.date && <p className="text-xs text-muted-foreground">{format(new Date(transactionSummary.first?.date), "dd/MM/yy")} - {format(new Date(transactionSummary.last?.date || ""), "dd/MM/yy")}</p>}
         </div>
-        <div className="col-span-6 row-span-1 border rounded-lg">
+        <div className="col-span-2 sm:col-span-6 row-span-1 border rounded-lg">
           <TransactionHistoryChart
             isLoading={isLoadingTransactions || isLoadingRecurringTransactions}
             chartData={transactionSummary.chartData} />
         </div>
-        <section className='h-full col-span-6 row-span-3 row-start-1 col-start-7'>
-          <ScrollArea className="h-[740px]">
+        <section className='h-full col-span-2 sm:col-span-6 xl:row-span-3 xl:row-start-1 xl:col-start-7'>
+          <ScrollArea className="xl:h-[740px]">
             <TransactionsTable
               transactions={transactionsFromId}
               isLoading={isLoadingTransactions}
