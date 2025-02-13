3662e0863b25bb5911e05628ccd371be0c189a5e
Thiago Elias <thiagoelias99@gmail.com>
2025-02-13 10:40:03 -0300

    toggle theme

--- planner-web-next/src/app/(authenticated)/_partials/header.tsx
+++ planner-web-next/src/app/(authenticated)/_partials/header.tsx
@@ -1,15 +1,19 @@
 import React from "react"
 import { SidebarTrigger } from "@/components/ui/sidebar"
 import UserButton from "./user-button"
+import { ToggleThemeButton } from "@/components/ui/toggle-theme"
 
 export default function AppHeader() {
   return (
-    <header className='flex flex-row justify-between items-center w-full py-2 pl-2 pr-4'>
+    <header className='flex flex-row justify-between items-center w-full py-2 pl-2 pr-4 bg-popover'>
       <div className='flex gap-2 items-center'>
         <SidebarTrigger />
         <h1>Planner</h1>
       </div>
-      <UserButton />
+      <div className='flex gap-2'>
+        <ToggleThemeButton />
+        <UserButton />
+      </div>
     </header>
   )
 }
